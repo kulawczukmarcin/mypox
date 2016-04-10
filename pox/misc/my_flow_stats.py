97c588fef6cb25033de47f4b6f547b0005a51de4
Marcin Kulawczuk <kulawczukmarcin@gmail.com>
2016-04-10 20:38:23 +0200

    Add function to apply flow stats to paths
    Function is not finished yet, because it lacks proper applying,
    question is how to do this to be nice and correct.

--- pox/misc/my_flow_stats.py
+++ pox/misc/my_flow_stats.py
@@ -37,6 +37,7 @@ paths = defaultdict(lambda:defaultdict(lambda:[]))
 log = core.getLogger()
 sws = {} # switches
 
+# get paths from my_topo_proactive module
 def get_paths():
   global sws
   from pox.forwarding.my_topo_proactive import switches_by_dpid
@@ -45,9 +46,11 @@ def get_paths():
     log.debug("NOT EQUAL - switches has changed since last time we checked")
     # to do - > add some clearing for stats
   else:
-    log.debug("EQUAL")
+    # log.debug("EQUAL")
+    pass
   for sw in sws.values():
-      log.debug("Switch %s, ports %s", dpidToStr(sw.dpid), sw.ports)
+      #log.debug("Switch %s, ports %s", dpidToStr(sw.dpid), sw.ports)
+      pass
 
   global paths
   from pox.forwarding.my_topo_proactive import all_cooked_paths
@@ -56,7 +59,28 @@ def get_paths():
     log.debug("NOT EQUAL - paths has changed since last time we checked")
     # to do - > add some clearing for stats
   else:
-    log.debug("EQUAL")
+    log.debug("EQUAL - switches has not changed since last time we checked")
+
+# when _handle_portstats_received will receive stats for port on switch
+# it will send them here to be applied for paths,
+# stats here are bytes sent by this port
+def apply_stats_to_paths(switch, port, stats):
+  global paths
+  log.debug("Checking switch %s port %s ", switch, port )
+  for src in sws.values():
+    for dst in sws.values():
+      for path in paths[src][dst]:
+        for switch_port_pair in path:
+          #log.debug("switch-port pair %s, %s", dpidToStr(switch_port_pair[0].dpid), switch_port_pair[1] )
+          if switch == dpidToStr(switch_port_pair[0].dpid) and port == switch_port_pair[1]:
+            # log.debug("switch-port pair %s, %s", dpidToStr(switch_port_pair[0].dpid), switch_port_pair[1] )
+            # log.debug(path)
+            #switch_port_pair.append(stats) -> this isn't working, what is better?
+            # to do -> how append stats?
+            # print stats
+            pass
+
+
 
 # handler for timer function that sends the requests to all the
 # switches connected to the controller.
@@ -65,32 +89,41 @@ def _timer_func ():
   for connection in core.openflow._connections.values():
     connection.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
     connection.send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
-  #log.debug("Sent %i flow/port stats request(s)", len(core.openflow._connections))
+  log.debug("Sent %i flow/port stats request(s)", len(core.openflow._connections))
 
 # handler to display flow statistics received in JSON format
 # structure of event.stats is defined by ofp_flow_stats()
 def _handle_flowstats_received (event):
   stats = flow_stats_to_list(event.stats)
-  # log.debug("FlowStatsReceived from %s: %s",
-  #   dpidToStr(event.connection.dpid), stats)
+  log.debug("FlowStatsReceived from %s: %s",
+    dpidToStr(event.connection.dpid), stats)
 
+
+
+
+  # not really needed
   # Get number of bytes/packets in flows for web traffic only
-  web_bytes = 0
-  web_flows = 0
-  web_packet = 0
-  for f in event.stats:
-    if f.match.tp_dst == 80 or f.match.tp_src == 80:
-      web_bytes += f.byte_count
-      web_packet += f.packet_count
-      web_flows += 1
-  # log.info("Web traffic from %s: %s bytes (%s packets) over %s flows",
+  # web_bytes = 0
+  # web_flows = 0
+  # web_packet = 0
+  # for f in event.stats:
+  #   if f.match.tp_dst == 80 or f.match.tp_src == 80:
+  #     web_bytes += f.byte_count
+  #     web_packet += f.packet_count
+  #     web_flows += 1
+  #log.info("Web traffic from %s: %s bytes (%s packets) over %s flows",
   #   dpidToStr(event.connection.dpid), web_bytes, web_packet, web_flows)
 
 # handler to display port statistics received in JSON format
 def _handle_portstats_received (event):
   stats = flow_stats_to_list(event.stats)
-  log.debug("PortStatsReceived from %s: %s",
-    dpidToStr(event.connection.dpid), stats)
+  # log.debug("PortStatsReceived from %s: %s",
+  #  dpidToStr(event.connection.dpid), stats)
+
+  for f in event.stats:
+    if int(f.port_no)<65534:
+      apply_stats_to_paths(dpidToStr(event.connection.dpid), f.port_no, f.tx_bytes)
+
 
 # main functiont to launch the module
 def launch ():
